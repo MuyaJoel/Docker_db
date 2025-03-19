8329934f8189920359aa76ecf804313f5e6bb1d8
Joel <joelmuya@outlook.com>
2025-03-19 19:51:30 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,87 @@
+import express from "express";
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const app = express();
+
+app.use(express.json());
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error("Database connection failed:", err.stack);
+  } else {
+    console.log("Connected to the database successfully!");
+    release();
+  }
+});
+
+app.get("/api/users", async (req, res) => {
+  try {
+    let result = await pool.query("SELECT * FROM students");
+    console.log("Data fetched successfully");
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error("Databse error:", error);
+    res.status(500).send("Database error");
+  }
+});
+
+app.get("/api/users/data", async (req, res) => {
+  try {
+    const { filter, value } = req.query;
+    let query = await pool.query("SELECT * FROM students");
+    console.log(`${filter}:${value}`);
+    if (filter && value) {
+      query = await pool.query(`SELECT * FROM students WHERE ${filter} = $1`, [
+        value,
+      ]);
+      return res.status(200).json(query.rows);
+    } else {
+      const query = await pool.query("SELECT * FROM students");
+      res.status(200).json(query.rows);
+    }
+  } catch (error) {
+    console.error("Database error:", error);
+    res.status(500).send("Database error");
+  }
+});
+
+app.get("/api/users/:id", async (req, res) => {
+  try {
+    let parseid = parseInt(req.params.id);
+    if (isNaN(parseid)) {
+      return res.status(400).send("Invalid id");
+    }
+    console.log(parseid);
+
+    let result = await pool.query("SELECT * FROM students WHERE s_id = $1", [
+      parseid,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).send("user not found");
+    }
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error("Database error:", error);
+    res.status(500).send("Database error");
+  }
+});
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => {
+  console.log(`Server is running on port, ${port}.`);
+});
